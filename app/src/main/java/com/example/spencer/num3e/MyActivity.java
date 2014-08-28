3d625cbcd359d3b101a20b6e1ab912577c24951a
SD <spencerdepas@gmail.com>
2014-08-27 23:44:30 -0400

    added multi number checking, not implemented yet

--- app/src/main/java/com/example/spencer/num3e/MyActivity.java
+++ app/src/main/java/com/example/spencer/num3e/MyActivity.java
@@ -26,6 +26,14 @@ public class MyActivity extends ActionBarActivity {
 
 
     String cellNumber;
+    String [] theNumbercombos = new String[70];
+    String [] numberReturnedFromtxtFile = new String[70];
+    int totalNumberCombos = 1;
+    int endOfArray = 0;
+    int increaseOnClick = 0;
+    final int startOfNumberIndexFromtxt = 23;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +51,15 @@ public class MyActivity extends ActionBarActivity {
             public void onClick(View v) {
 
                 cellNumber = editText.getText().toString();
+                increaseOnClick ++;
 
-                AS task = new AS();
+
+
+                //this is to get the combinations of numbers to check with the txt file
+                GetCombinations task = new GetCombinations();
                 task.execute(new String[] { cellNumber });
+
+
 
             }
     });
@@ -55,8 +69,161 @@ public class MyActivity extends ActionBarActivity {
 
 
 
+    private class GetCombinations extends AsyncTask<String, Void, String> {
+
+        //this is to get the combinations of numbers to check with the txt file
+        @Override
+        protected String doInBackground(String... urls) {
 
 
+
+            int endBlockNumberLength = cellNumber.length() - 1;
+            int combinationsPerBlock = 2;
+
+
+            theNumbercombos[0] = cellNumber;
+
+            //this is what creates every number combination of the input number
+            // 123
+            //12
+            //23
+            //1 , 2, 3
+            for(int i = 0, z = 0, k = 1; i < theNumbercombos.length && z < combinationsPerBlock && endBlockNumberLength > 1; i++, k++ ){
+
+                theNumbercombos[k] = cellNumber.substring(i, i + endBlockNumberLength);
+
+
+                if(i + endBlockNumberLength == cellNumber.length()){
+                    endBlockNumberLength --;
+                    i = -1;
+                }
+            }
+
+            //tells you the end of the array
+            for(int i = 0; i < theNumbercombos.length; i++ ){
+                if(theNumbercombos[i] != null){
+
+                }else if(endOfArray == 0){
+                    endOfArray = i - 1 ;
+                }
+
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this is for buffered reader and going through txt files ect
+            AS task = new AS();
+            task.execute();
+        }
+
+
+
+
+    }
+
+
+
+
+    // buffered reader shit, this is what enters the txt file and returns words
+    private class AS extends AsyncTask<String, Void, String> {
+
+
+        public String sCurrentLine = "";
+        String sCurrentLineNumberOnly = "";
+        String sCurrentLineWordOnly = "";
+        final int startOfNumberIndex = 23;
+
+        public BufferedReader br;
+        final AssetManager assetManager = getAssets();
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+
+
+
+
+
+                InputStream input = assetManager.open("wordlist.txt");
+
+                br = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+
+                for (int u = 0;(sCurrentLine = br.readLine()) != null; ) {
+
+                    if (theNumbercombos[u] == null) {
+                        break;
+                    }
+
+                    if (theNumbercombos[u].equals(sCurrentLine.substring(startOfNumberIndexFromtxt))) {
+                        int endOfWordIndex = sCurrentLine.indexOf(' ');
+                        numberReturnedFromtxtFile[u] = sCurrentLine.substring(0, endOfWordIndex);
+                        u++;
+                        break;
+                    }
+
+
+                }
+
+                   /* if(theNumbercombos[i] == null){
+                        break;
+                    }
+
+                    sCurrentLineNumberOnly = sCurrentLine.substring(startOfNumberIndex);
+
+                    if(theNumbercombos[0].equals(sCurrentLineNumberOnly)){
+
+                        int endOfWord =  sCurrentLine.indexOf(' ');
+                        sCurrentLineWordOnly = sCurrentLine.substring(0, endOfWord);
+                        break;
+                    }*/
+
+
+
+                input.close();
+
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            toaster(sCurrentLineNumberOnly);
+            final EditText editText = (EditText) findViewById(R.id.editText);
+            //editText.setText(numberReturnedFromtxtFile[0]);
+            //editText.setText(Integer.toString(increaseOnClick));
+
+            editText.setText(numberReturnedFromtxtFile[0]);
+            //editText.setText(theNumbercombos[0]);
+
+
+        }
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 
@@ -79,78 +246,8 @@ public class MyActivity extends ActionBarActivity {
         int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-        }
-
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
-
-    private class AS extends AsyncTask<String, Void, String> {
-
-
-        public String sCurrentLine = "";
-        String sCurrentLineNumberOnly = "";
-        String sCurrentLineWordOnly = "";
-        final int startOfNumberIndex = 23;
-
-        public BufferedReader br;
-        final AssetManager assetManager = getAssets();
-
-
-        @Override
-        protected String doInBackground(String... urls) {
-
-            try {
-
-                int i = 0;
-
-                InputStream input = assetManager.open("wordlist.txt");
-
-                br = new BufferedReader(new InputStreamReader(input, "UTF-8"));
-
-                while ((sCurrentLine = br.readLine()) != null){
-
-                    sCurrentLineNumberOnly = sCurrentLine.substring(startOfNumberIndex);
-
-                    if(cellNumber.equals(sCurrentLineNumberOnly)){
-
-                        int endOfWord =  sCurrentLine.indexOf(' ');
-                        sCurrentLineWordOnly = sCurrentLine.substring(0, endOfWord);
-                        break;
-                    }
-                }
-
-                input.close();
-
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            toaster(sCurrentLineNumberOnly);
-            final EditText editText = (EditText) findViewById(R.id.editText);
-            editText.setText(sCurrentLineWordOnly);
-        }
-
-
-
-
-    }
 
 
 
